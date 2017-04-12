82ff9bd52938c18f6efb90c52f20aa3b68fd5c79
Gene Doyel <gdoyel@protonmail.com>
2017-04-12 10:20:12 -0500

    Adds relations between calendar date and calendar

--- db/migrate/20170403122345_create_calendar_dates.rb
+++ db/migrate/20170403122345_create_calendar_dates.rb
@@ -1,7 +1,7 @@
 class CreateCalendarDates < ActiveRecord::Migration[5.1]
   def change
     create_table :calendar_dates do |t|
-      t.integer :service_id, presence: true
+      t.string :service_id, presence: true
       t.datetime :date, presence: true
       t.integer :exception_type, presence: true
 
