98a4835c2f40430e84fe4a33dedee809786744ea
Chase Clettenberg <clettenberg@gmail.com>
2017-04-21 22:11:38 -0500

    Fix validations related to lat/lon fields

--- spec/models/shape_spec.rb
+++ spec/models/shape_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 describe Shape, type: :model do
   describe "Validations" do
-    it { should validate_presence_of :shape_pt_lat }
-    it { should validate_presence_of :shape_pt_lon }
+    it { should validate_presence_of :shape_pt_latlon }
     it { should validate_presence_of :shape_pt_sequence }
   end
 end
