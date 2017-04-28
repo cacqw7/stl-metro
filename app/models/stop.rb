ea67ad1e9a70477039853136094d9f59b8a482fe
Chase Clettenberg <clettenberg@gmail.com>
2017-04-27 21:15:02 -0500

    Make a ‘has_many’ relationship between stops and trips

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,10 +1,11 @@
 class Stop < ApplicationRecord
   has_many :stop_times, primary_key: 'stop_id'
+  has_many :trips, through: :stop_times
 
   validates_presence_of :stop_name,
                         :stop_latlon
 
   def route_short_names
-    stop_times.includes(trip: :route).pluck(:route_short_name).uniq
+    trips.includes(:route).pluck(:route_short_name).uniq
   end
 end
